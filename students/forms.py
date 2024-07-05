87d87d4fd89fcc672c59063b248fc8fad52fc052
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-07-05 20:27:11 +0800

    Added Add-Student-Form

--- students/forms.py
+++ students/forms.py
@@ -1,23 +1,23 @@
 from django import forms
 from .models import Student
 
-class StudentForm(forms.ModalForm):
+class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        field = ['student_number', 'first_name', 'last_name', 'email', 'course', 'gpa']
+        fields = ['Student_number', 'first_name', 'last_name', 'email', 'course', 'gpa']  # Corrected 'field' to 'fields'
         labels = {
-            'student_number': 'Student Number', 
-            'first_name': 'First Name', 
-            'last_name': 'Last Name', 
-            'email': 'Email', 
-            'course': 'Course', 
+            'Student_number': 'Student Number',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'course': 'Course',
             'gpa': 'GPA',
         }
         widgets = {
-            'student_number': forms.NumberInput(attrs={'class': 'form-control'}), 
-            'first_name': forms.TextInput(attrs={'class': 'form-control'}), 
-            'last_name': forms.TextInput(attrs={'class': 'form-control'}), 
-            'email': forms.EmailInput(attrs={'class': 'form-control'}), 
-            'course': forms.TextInput(attrs={'class': 'form-control'}), 
+            'Student_number': forms.NumberInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'course': forms.TextInput(attrs={'class': 'form-control'}),
             'gpa': forms.NumberInput(attrs={'class': 'form-control'}),
         }
--- students/views.py
+++ students/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from .models import Student
@@ -7,19 +7,19 @@ from .forms import StudentForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'students/index.html',{
+    return render(request, 'students/index.html', {
         'students': Student.objects.all()
     })
 
 def view_student(request, id):
     student = Student.objects.get(pk=id)
     return HttpResponseRedirect(reverse('index'))
-    
+
 def add(request):
     if request.method == 'POST':
         form = StudentForm(request.POST)
         if form.is_valid():
-            new_student_number = form.cleaned_data['student_number']
+            new_student_number = form.cleaned_data['Student_number']
             new_first_name = form.cleaned_data['first_name']
             new_last_name = form.cleaned_data['last_name']
             new_email = form.cleaned_data['email']
@@ -27,20 +27,21 @@ def add(request):
             new_gpa = form.cleaned_data['gpa']
 
             new_student = Student(
-                student_number = new_student_number,
-                first_name = new_first_name,
-                last_name = new_last_name,
-                email = new_email,
-                course = new_course,
-                gpa = new_gpa
+                Student_number=new_student_number,
+                first_name=new_first_name,
+                last_name=new_last_name,
+                email=new_email,
+                course=new_course,
+                gpa=new_gpa
             )
             new_student.save()
-            return render(request, 'student/add/html',{
+            return render(request, 'students/add.html', {
                 'form': StudentForm(),
                 'success': True
             })
-        else:
-            form = StudentForm()
-        return render(request, 'student/add/html',{
-            'form': StudentForm()
-        })
+    else:
+        form = StudentForm()
+
+    return render(request, 'students/add.html', {
+        'form': form
+    })
